a26f4f136954a7468a126f356cab5189a81b422a
Aiko Radlingmayr <radlingmayr.a@gmail.com>
2020-12-07 22:03:47 +0100

    create empty console app

---
+++ HoHeCo/HoHeCo-Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HoHeCo_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
